6f3125d543bacf2db59e45dc7ecbe54a4b45af02
Berndoe <88336484+Berndoe@users.noreply.github.com>
2025-01-11 00:19:17 +0000

    adding rollup

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/bernd-portfolio/'
+  base: '/bernd-portfolio/',
+  build: {
+    rollupOptions: {
+      external: ['react-icons'],
+    },
+  },
 })
